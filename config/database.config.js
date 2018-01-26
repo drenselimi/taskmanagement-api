6baedb3ce2659d620cd653a1051078310e9b8e2a
Ferdi <ferdi.bejtullahu@gmail.com>
2018-01-26 18:45:13 +0100

    db connection

--- config/database.config.js
+++ config/database.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     //  url: 'mongodb://127.0.0.1:27017/taskmanagement',
-    url: process.env[".MONGODB_ADDON_URI"],
+    url: process.env["MONGODB_ADDON_URI"],
     auth: {
         user: process.env["MONGODB_ADDON_USER"],
         password: process.env["MONGODB_ADDON_PASSWORD"]
