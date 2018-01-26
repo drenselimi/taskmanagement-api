d8e17c41e4426f934840494b041d75b9003537bb
Ferdi <ferdi.bejtullahu@gmail.com>
2018-01-26 15:58:38 +0100

    connection string

--- config/database.config.js
+++ config/database.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-    url: 'mongodb://127.0.0.1:27017/taskmanagement',
-    auth: {
+   //  url: 'mongodb://127.0.0.1:27017/taskmanagement',
+   url: 'mongodb://172.30.71.231:27017/taskmanagement', 
+   auth: {
         user: process.env.OPENSHIFT_MONGODB_DB_USERNAME,
         password: process.env.OPENSHIFT_MONGODB_DB_PASSWORD
     }
